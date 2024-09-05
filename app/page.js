704face7f78c56fe44f25c37859f01853650c148
aitizaz <atz.softprgmr@gmail.com>
2024-09-05 13:15:00 +0500

    Biellavita first commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <div className="header-container-left">monogram</div>
+        <div className="header-container-right">
+          <div className="button-group-container">
+            <button className="cicular-button">user</button>
+            <button className="cicular-button">menu</button>
+          </div>
+        </div>
+      </header>
+    </main>
+  );
+}
