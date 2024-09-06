c74d3b6dac12f5fa84918e7234af7fbbda290cb0
aitizaz <atz.softprgmr@gmail.com>
2024-09-06 10:35:19 +0500

    added changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-export default defineConfig({
+const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true,
+    unoptimized: true, // GitHub Pages doesn't support optimized images
   },
   basePath: "/BiellavitaWebsite", // Add your repo name here for GitHub Pages
   assetPrefix: "/BiellavitaWebsite/", // This is necessary for GitHub Pages
-});
+};
+module.exports = nextConfig;
