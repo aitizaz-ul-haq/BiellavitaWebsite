d0b5a3574f28a13fe0d648281a97386704664c08
aitizaz <atz.softprgmr@gmail.com>
2024-11-18 10:28:22 +0500

    added changes

--- app/components/pagecomponents/headercomponents/headerleftsection.js
+++ app/components/pagecomponents/headercomponents/headerleftsection.js
@@ -5,7 +5,7 @@ export default function HeaderLeftSection() {
     <div className="header-container-left">
       {" "}
       <Image
-        src="/Monogram.png" // The path relative to the "public" folder
+        src="/images/Monogram.png" // The path relative to the "public" folder
         alt="My Awesome Image"
         width={55} // Width of the image
         height={55} // Height of the image
